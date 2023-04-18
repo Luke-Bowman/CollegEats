e5cf6da61130bd68d35466f295a20aacda2835e0
Luke-Bowman <lkb5453@psu.edu>
2023-04-17 20:34:01 -0400

    menu V4 updated main page

---
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/GroceryList.java
@@ -0,0 +1,30 @@
+package edu.psu.ist.hcdd340.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class GroceryList extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grocery_list);
+
+        listView = findViewById(R.id.list_view);
+        items = new ArrayList<>();
+        items.add("sugar");
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/MainActivity.java
@@ -3,13 +3,21 @@ package edu.psu.ist.hcdd340.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 }
