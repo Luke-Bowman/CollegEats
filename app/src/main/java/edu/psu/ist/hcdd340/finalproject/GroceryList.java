9b34deabe05e41d13db91cae2872882c4d17f7b3
Cora-Lena <cba5270@psu.edu>
2023-04-30 21:01:31 -0400

    recipie page updated. (grocery_list.xml)

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/GroceryList.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/GroceryList.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 
 public class GroceryList extends AppCompatActivity {
