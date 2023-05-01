0994630afc07f5d60e1721016cccf4fb120a21c7
lukeSegner <las6655@psu.edu>
2023-05-01 19:58:53 -0400

    yo

---
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/Recpies.java
@@ -0,0 +1,24 @@
+package edu.psu.ist.hcdd340.finalproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Recpies extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipes_from_list);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if
+    }
+}
