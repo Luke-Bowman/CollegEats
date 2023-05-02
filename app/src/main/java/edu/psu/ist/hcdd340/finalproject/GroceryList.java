721e69199c416b525d81999fd4d6269978d3a642
lukeSegner <las6655@psu.edu>
2023-05-01 21:27:42 -0400

    yo

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/GroceryList.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/GroceryList.java
@@ -1,8 +1,12 @@
 package edu.psu.ist.hcdd340.finalproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,5 +24,45 @@ public class GroceryList extends AppCompatActivity {
         setContentView(R.layout.grocery_list);
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuId = item.getItemId();
+
+        if (menuId == R.id.menu_login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuId == R.id.menu_register) {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuId == R.id.menu_recipes){
+            Intent intent = new Intent(this, FoodListActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuId == R.id.menu_myrecipes){
+            Intent intent = new Intent(this, FoodProfile.class);
+            startActivity(intent);
+            return true;
+        }
+
+        else if  (menuId == R.id.menu_info) {
+            AlertDialog.Builder d = new AlertDialog.Builder(this);
+            d.setTitle(R.string.info_alert_title);
+            d.setMessage(R.string.info_alert_message);
+            d.setPositiveButton(android.R.string.ok, null);
+            d.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
