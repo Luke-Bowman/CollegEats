969d3cd6b44b13dc5e581ab5559ec05911ee05c6
lukeSegner <las6655@psu.edu>
2023-05-01 21:03:17 -0400

    yo

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/FoodListActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/FoodListActivity.java
@@ -1,16 +1,20 @@
 package edu.psu.ist.hcdd340.finalproject;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FoodListActivity extends AppCompatActivity {
+public class FoodListActivity extends AppCompatActivity implements View.OnClickListener{
     private final FoodData[] FOOD_LIST = {
             new FoodData("Shrimp Scampi", R.drawable.resized_shrimp_scampi),
             new FoodData("Cheese Burger", R.drawable.resized_burger),
@@ -22,6 +26,7 @@ public class FoodListActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private FoodListAdapter mAdapter;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,17 @@ public class FoodListActivity extends AppCompatActivity {
         mAdapter = new FoodListAdapter(this, FOOD_LIST);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        sharedPreferences = getSharedPreferences(RegisterActivity.SHARED_PREF_NAME, MODE_PRIVATE);
+    }
+    @Override
+    public void onClick(View view){
+        int id = view.getId();
+        if (id == R.id.food_recipe_button) {
+            Intent intent = new Intent(this, Recpies.class);
+            startActivity(intent);
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/Recpies.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/Recpies.java
@@ -1,12 +1,18 @@
 package edu.psu.ist.hcdd340.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Recpies extends AppCompatActivity implements View.OnClickListener {
+public class Recpies extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +23,47 @@ public class Recpies extends AppCompatActivity implements View.OnClickListener {
     }
 
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
     @Override
-    public void onClick(View view) {
-        if
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int menuID = item.getItemId();
+
+        if (menuID == R.id.menu_login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuID == R.id.menu_register) {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuID == R.id.menu_recipes){
+            Intent intent = new Intent(this, FoodListActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuID == R.id.menu_myrecipes){
+            Intent intent = new Intent(this, FoodProfile.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (menuID == R.id.menu_grocerylist) {
+            Intent intent = new Intent(this, GroceryList.class);
+            startActivity(intent);
+            return true;
+        }
+        else if  (menuID == R.id.menu_info) {
+            AlertDialog.Builder d = new AlertDialog.Builder(this);
+            d.setTitle(R.string.info_alert_title);
+            d.setMessage(R.string.info_alert_message);
+            d.setPositiveButton(android.R.string.ok, null);
+            d.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
